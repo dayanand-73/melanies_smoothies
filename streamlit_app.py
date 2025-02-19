734cb30c0cbc1dc79769b8b33e0d263cd52e6164
dayanand-73 <dayab4@gmail.com>
2025-02-19 11:19:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ if ingredients_list:
 
 import requests
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/6")
 st.text(smoothiefroot_response)
